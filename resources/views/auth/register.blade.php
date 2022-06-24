6c6c54b2cfb5c62064b8b4ae13abadf47a866060
datintambunan <105769261+datintambunan@users.noreply.github.com>
2022-06-24 21:06:47 +0700

    laravel-quiz_datin
    quis laravel datin
    laravel pertemuan 15 quis

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Alamat') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="alamat" type="Alamat" class="form-control @error('user_alamat') is-invalid @enderror" name="user_alamat" value="{{ old('user_alamat') }}" required autocomplete="email">
+
+                                @error('user_alamat')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="hp" class="col-md-4 col-form-label text-md-end">{{ __('Hp') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="hp" type="HP" class="form-control @error('user_hp') is-invalid @enderror" name="user_hp" value="{{ old('user_hp') }}" required autocomplete="hp">
+
+                                @error('user_hp')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="pos" class="col-md-4 col-form-label text-md-end">{{ __('Pos') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="pos" type="Pos" class="form-control @error('user_pos') is-invalid @enderror" name="user_pos" value="{{ old('user_pos') }}" required autocomplete="pos">
+
+                                @error('user_pos')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/golongan/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+
+        <h3>
+            Data Golongan
+            <a class="btn btn-primary btn-sm float-end" href="{{ url('golongan/create') }}">Tambah Data</a>
+            <a class="btn btn-danger btn-sm float-end" href="{{ url('home') }}">Kembali</a>
+        </h3>
+
+        <table class="table table-bordered">
+            <tr>
+                <td>Kode Golongan</td>
+                <td>Nama Golongan</td>
+                <td>EDIT</td>
+                <td>HAPUS</td>
+            </tr>
+            @foreach ($rows as $row)
+                <tr>
+              
+                    <td>{{ $row->gol_kode }}</td>
+                    <td>{{ $row->gol_nama }}</td>
+                    <td><a href="{{ url('golongan/' . $row->gol_id . '/edit') }}">Edit</a></td>
+                    <td>
+                        <form action="{{ url('golongan/' . $row->gol_id) }}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button>Hapus</button>
+                        </form>
+
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
+@endsection
